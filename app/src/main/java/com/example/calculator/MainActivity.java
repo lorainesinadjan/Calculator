0e27464bad386e5a3ac9be7a121f6adb35cc7f47
lorainesinadjan <s2020285@usls.edu.ph>
2022-11-20 21:45:14 +0800

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,52 +15,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Add(View v){
-        EditText et1 = (EditText)findViewById(R.id.editTextNumber);
-        EditText et2 = (EditText)findViewById(R.id.editTextNumber2);
-        EditText et3 = (EditText)findViewById(R.id.editTextNumber3);
+        EditText et1 =(EditText)findViewById(R.id.editTextNumber);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
+        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
 
 
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int result = n1+n2;
+        int n1 =Integer.parseInt(et1.getText().toString());
+        int n2 =Integer.parseInt(et2.getText().toString());
+        int n3 =Integer.parseInt(et3.getText().toString());
+        int result = n1+n2+n3;
 
-        et3.setText("Total Value = " + result);
+        et4.setText("Total Value = " + result);
     }
 
     public void Subtract(View v){
-        EditText et1 = (EditText)findViewById(R.id.editTextNumber);
-        EditText et2 = (EditText)findViewById(R.id.editTextNumber2);
-        EditText et3 = (EditText)findViewById(R.id.editTextNumber3);
+        EditText et1 =(EditText)findViewById(R.id.editTextNumber);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
+        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
 
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int result = n1-n2;
+        int n1 =Integer.parseInt(et1.getText().toString());
+        int n2 =Integer.parseInt(et2.getText().toString());
+        int n3 =Integer.parseInt(et3.getText().toString());
+        int result = n1-n2-n3;
 
-        et3.setText("Subtract Value = " + result);
+        et4.setText("Subtract Value = " + result);
     }
 
     public void Multiply(View v){
-        EditText et1 = (EditText)findViewById(R.id.editTextNumber);
-        EditText et2 = (EditText)findViewById(R.id.editTextNumber2);
-        EditText et3 = (EditText)findViewById(R.id.editTextNumber3);
+        EditText et1 =(EditText)findViewById(R.id.editTextNumber);
+        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
+        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
+        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
 
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int result = n1*n2;
 
-        et3.setText("Multiply Value =  " + result);
+        int n1 =Integer.parseInt(et1.getText().toString());
+        int n2 =Integer.parseInt(et2.getText().toString());
+        int n3 =Integer.parseInt(et3.getText().toString());
+        int result = n1*n2*n3;
+
+        et4.setText("Multiply Value = " + result);
     }
 
     public void Divide(View v){
-        EditText et1 = (EditText)findViewById(R.id.editTextNumber);
-        EditText et2 = (EditText)findViewById(R.id.editTextNumber2);
-        EditText et3 = (EditText)findViewById(R.id.editTextNumber3);
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber3);
+        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
 
         int n1 = Integer.parseInt(et1.getText().toString());
         int n2 = Integer.parseInt(et2.getText().toString());
-        int result = n1/n2;
+        int n3 = Integer.parseInt(et3.getText().toString());
+        int result = n1 / n2 / n3;
 
-        et3.setText("Divide Value = " + result);
+        et4.setText("Divide Value = " + result);
     }
-
 }
